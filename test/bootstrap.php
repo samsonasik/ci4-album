a7101a97b5c5b6a04c3e174f6a186f8cab341530
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-11 21:59:10 +0700

    retry

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 
 require_once 'vendor/codeigniter4/framework/system/Test/bootstrap.php';
-$routes = Config\Services::routes();
+
+$routes = \Config\Services::routes();
 $routes->getRoutes('*');
