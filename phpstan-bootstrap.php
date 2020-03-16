1589450278a44a8f6ffb2d53bab93e1d17f02176
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-16 21:36:10 +0700

    clean up

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-$pathsPath = 'vendor/codeigniter4/framework/app/Config/Paths.php';
-require $pathsPath;
+require 'vendor/codeigniter4/framework/app/Config/Paths.php';
 $paths = new Config\Paths();
 require_once 'vendor/codeigniter4/framework/system/bootstrap.php';
