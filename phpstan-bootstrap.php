c234877f87204b0c5f2497280cd735b3dfef2069
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-16 21:12:45 +0700

    add phpstan bootstrap

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+$pathsPath = 'vendor/codeigniter4/framework/app/Config/Paths.php';
+require $pathsPath;
+$paths = new Config\Paths();
+require_once 'vendor/codeigniter4/framework/system/bootstrap.php';
