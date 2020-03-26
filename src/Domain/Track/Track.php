478283301cca9c9d81dc29df2aab8ae1292550a0
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-26 19:31:26 +0700

    handle album_id key not exists

--- src/Domain/Track/Track.php
+++ src/Domain/Track/Track.php
@@ -1,6 +1,7 @@
 <?php namespace Album\Domain\Track;
 
 use CodeIgniter\Entity;
+use InvalidArgumentException;
 
 /**
  * @property int $id
@@ -11,7 +12,10 @@ class Track extends Entity
 {
 	public function __construct(array $data = null)
 	{
-		$data['album_id'] = $data['album_id'] ?? 0;
+		if (! isset($data['album_id']))
+		{
+			throw new InvalidArgumentException('album_id key must be exists');
+		}
 		$data['title']    = $data['title'] ?? '';
 
 		parent::__construct($data);
