e011ed6676f16a2984f6f362c5bd11c0b0c057fc
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-27 13:22:55 +0700

    some styling

--- src/Views/album/add.php
+++ src/Views/album/add.php
@@ -19,25 +19,25 @@ echo form_open('album/add');
 ?>
 
 <div class="form-group">
-    <?php
-    echo form_label('Artist', 'artist', ['for' => 'artist']);
-    echo form_input('artist', set_value('artist'), ['class' => 'form-control']);
-    ?>
-    <span class="error text-danger"><?php echo $errors['artist'] ?? '' ?></span>
+	<?php
+	echo form_label('Artist', 'artist', ['for' => 'artist']);
+	echo form_input('artist', set_value('artist'), ['class' => 'form-control']);
+	?>
+	<span class="error text-danger"><?php echo $errors['artist'] ?? '' ?></span>
 </div>
 
 <div class="form-group">
-    <?php
-    echo form_label('Title', 'title', ['for' => 'title']);
-    echo form_input('title', set_value('title'), ['class' => 'form-control']);
-    ?>
-    <span class="error text-danger"><?php echo $errors['title'] ?? '' ?></span>
+	<?php
+	echo form_label('Title', 'title', ['for' => 'title']);
+	echo form_input('title', set_value('title'), ['class' => 'form-control']);
+	?>
+	<span class="error text-danger"><?php echo $errors['title'] ?? '' ?></span>
 </div>
 
 <div class="form-group">
-    <?php
-        echo form_submit('Save', 'Save New Album', ['class' => 'btn btn-primary']);
-    ?>
+	<?php
+		echo form_submit('Save', 'Save New Album', ['class' => 'btn btn-primary']);
+	?>
 </div>
 
 <?php echo form_close(); ?>
--- src/Views/album/edit.php
+++ src/Views/album/edit.php
@@ -19,25 +19,25 @@ echo form_open(sprintf('album/edit/%d', $album->id));
 echo form_hidden('id', set_value('id', $album->id));
 ?>
 <div class="form-group">
-    <?php
-    echo form_label('Artist', 'artist', ['for' => 'artist']);
-    echo form_input('artist', set_value('artist', $album->artist), ['class' => 'form-control']);
-    ?>
-    <span class="error text-danger"><?php echo $errors['artist'] ?? '' ?></span>
+	<?php
+	echo form_label('Artist', 'artist', ['for' => 'artist']);
+	echo form_input('artist', set_value('artist', $album->artist), ['class' => 'form-control']);
+	?>
+	<span class="error text-danger"><?php echo $errors['artist'] ?? '' ?></span>
 </div>
 
 <div class="form-group">
-    <?php
-    echo form_label('Title', 'title', ['for' => 'title']);
-    echo form_input('title', set_value('title', $album->title), ['class' => 'form-control']);
-    ?>
-    <span class="error text-danger"><?php echo $errors['title'] ?? '' ?></span>
+	<?php
+	echo form_label('Title', 'title', ['for' => 'title']);
+	echo form_input('title', set_value('title', $album->title), ['class' => 'form-control']);
+	?>
+	<span class="error text-danger"><?php echo $errors['title'] ?? '' ?></span>
 </div>
 
 <div class="form-group">
-    <?php
-        echo form_submit('Save', 'Update Album', ['class' => 'btn btn-primary']);
-    ?>
+	<?php
+		echo form_submit('Save', 'Update Album', ['class' => 'btn btn-primary']);
+	?>
 </div>
 <?php  echo form_close(); ?>
 
