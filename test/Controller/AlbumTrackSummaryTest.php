79697faec13b4ee61739a81ec5404fe5ab8a228f
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-28 04:36:01 +0700

    more tests

--- test/Controller/AlbumTrackSummaryTest.php
+++ test/Controller/AlbumTrackSummaryTest.php
@@ -5,6 +5,7 @@ use Album\Database\Seeds\AlbumSeeder;
 use Album\Database\Seeds\TrackSeeder;
 use CodeIgniter\Test\CIDatabaseTestCase;
 use CodeIgniter\Test\ControllerTester;
+use Config\Database;
 
 class AlbumTrackSummaryTest extends CIDatabaseTestCase
 {
@@ -17,11 +18,25 @@ class AlbumTrackSummaryTest extends CIDatabaseTestCase
 		TrackSeeder::class,
 	];
 
-	public function testTotalSongSummary()
+	public function testTotalSongSummaryHasNoData()
+	{
+		Database::connect()->disableForeignKeyChecks();
+		Database::connect()->table('album')->truncate();
+		Database::connect()->enableForeignKeyChecks();
+
+		$result = $this->controller(AlbumTrackSummary::class)
+						->execute('totalsong');
+
+		$this->assertTrue($result->isOK());
+		$this->assertTrue($result->see('No album track summary found.'));
+	}
+
+	public function testTotalSongSummaryHasData()
 	{
 		$result = $this->controller(AlbumTrackSummary::class)
 					   ->execute('totalsong');
 
+		$this->assertTrue($result->isOK());
 		$this->assertRegExp('/Sheila On 7<\/td>\s{0,}\n\s{0,}<td>1<\/td>/', $result->getBody());
 	}
 }
