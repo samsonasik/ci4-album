c2c911ee7c001a03ab2cfecafc26c866ebbb2c01
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-03-27 07:19:32 +0700

    disable - enable on truncate album table

--- test/Controller/AlbumTest.php
+++ test/Controller/AlbumTest.php
@@ -19,15 +19,11 @@ class AlbumTest extends CIDatabaseTestCase
 	protected $namespace = 'Album';
 	protected $seed      = AlbumSeeder::class;
 
-	protected function setUp(): void
-	{
-		parent::setUp();
-		\Config\Database::connect()->disableForeignKeyChecks();
-	}
-
 	public function testIndexAlbumHasNoData()
 	{
+		Database::connect()->disableForeignKeyChecks();
 		Database::connect()->table('album')->truncate();
+		Database::connect()->enableForeignKeyChecks();
 
 		$result = $this->controller(Album::class)
 						->execute('index');
@@ -187,11 +183,5 @@ class AlbumTest extends CIDatabaseTestCase
 					   ->execute('delete', 1);
 
 		$this->assertTrue($result->isRedirect());
-	}
-
-	public function tearDown(): void
-	{
-		parent::tearDown();
-		\Config\Database::connect()->enableForeignKeyChecks();
 	}
 }
