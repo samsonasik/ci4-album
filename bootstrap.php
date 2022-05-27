78bbe8b2cff0f3aeb5c52f8512f3570368dddc64
Abdul Malik Ikhsan <samsonasik@gmail.com>
2022-05-28 04:42:34 +0700

    rectify

--- bootstrap.php
+++ bootstrap.php
@@ -12,14 +12,6 @@
 use App\Controllers\BaseController;
 use Config\Paths;
 
-/**
- * This file is part of samsonasik/ci4-album.
- *
- * (c) Abdul Malik Ikhsan
- *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
- */
 require __DIR__ . '/vendor/codeigniter4/framework/app/Config/Paths.php';
 $paths = new Paths();
 require_once __DIR__ . '/vendor/codeigniter4/framework/system/bootstrap.php';
