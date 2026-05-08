6695a81588138ed771e0a89f08a6ea9a18b7ddca
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-05-08 19:33:00 +0700

    cs fix

--- src/Models/TrackModel.php
+++ src/Models/TrackModel.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please view
  * the LICENSE file that was distributed with this source code.
  */
+
 namespace Album\Models;
 
 use Album\Domain\Track\Track;
