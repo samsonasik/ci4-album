3f9e263703174cab03e27ccf6862a59e05705855
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-06-19 07:01:55 +0700

    rectify

--- pyrameter.php
+++ pyrameter.php
@@ -7,7 +7,7 @@ use Boundwize\Pyrameter\Config\PyrameterConfig;
 return PyrameterConfig::defaults()
     ->targetShape(
         unit: ['min' => 10],
-        integration: ['max' => 40],
         functional: ['max' => 55],
+        integration: ['max' => 40],
     )
     ->failOnViolation();
